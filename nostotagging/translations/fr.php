9fb63807861342f6d0f7303c1190c1570739a45d
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-09-10 16:00:12 +0300

    Added a French localisation pack for the module.

---
+++ nostotagging/translations/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{nostotagging}prestashop>nostotagging_285cb38d23cdf651e85a327334a9e67d'] = 'Les données du compte doivent être configurées avant l\'utilisation de ce module.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_ec222200fe23009388901af3ec23a7de'] = 'Recommandations Nosto';
+$_MODULE['<{nostotagging}prestashop>nostotagging_70b3fe372fa6162e116fd7244c9d1bba'] = 'Intègre les fonctionnalités de marketing automatisé Nosto';
+$_MODULE['<{nostotagging}prestashop>nostotagging_835728c5375d41d626cd2a7341b7afba'] = 'L\'adresse du serveur n\'est pas une URL valide';
+$_MODULE['<{nostotagging}prestashop>nostotagging_5eabe0125561efcebc8dc9c08dceada9'] = 'L\'adresse du serveur ne doit pas contenir le protocole http ou https';
+$_MODULE['<{nostotagging}prestashop>nostotagging_dc5be3e3ca8cc71dd53e412ad625ecd4'] = 'Le nom du compte doit être renseigné';
+$_MODULE['<{nostotagging}prestashop>nostotagging_310d1e088e81ab4bbc800099de257ef2'] = 'Les paramètres d\'utilisation des bandeaux de recommandation par défaut sont incorrects.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_e60263ac381ae4a9ad460a5c706b9b5a'] = 'Paramètres enregistrés';
+$_MODULE['<{nostotagging}prestashop>nostotagging_d471d1247d1daf057584fe33cabe647b'] = 'Vous n\'avez pas configuré de compte Nosto. Veuillez visiter www.nosto.com pour créer un compte et commencer à utiliser Nosto.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_52f4393e1b52ba63e27310ca92ba098c'] = 'Paramètres généraux';
+$_MODULE['<{nostotagging}prestashop>nostotagging_227339e1efccf37069ddfd86c4e48aef'] = 'Adresse du serveur';
+$_MODULE['<{nostotagging}prestashop>nostotagging_230af5213cb6810e5e5b7d0d9eb4f9e7'] = 'L\'adresse du serveur pour les fonctionnalités de marketing automatisé Nosto';
+$_MODULE['<{nostotagging}prestashop>nostotagging_aa90c53280582695f3d4b8ac2ec17a8b'] = 'Nom du compte';
+$_MODULE['<{nostotagging}prestashop>nostotagging_c125e31a0f4349440c57b488af62c969'] = 'Votre nom de compte Nosto';
+$_MODULE['<{nostotagging}prestashop>nostotagging_33f04ded50801147c28937fe34c61eef'] = 'Utilisez les bandeaux de recommandation Nosto par défaut';
+$_MODULE['<{nostotagging}prestashop>nostotagging_58db9b260bd6b54632b12b602f809e5c'] = 'Utilisez les bandeaux de recommandation Nosto par défaut pour afficher les recommandations de produits';
+$_MODULE['<{nostotagging}prestashop>nostotagging_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activé';
+$_MODULE['<{nostotagging}prestashop>nostotagging_b9f5c797ebbf55adccdd8539a65a0241'] = 'Désactivé';
+$_MODULE['<{nostotagging}prestashop>nostotagging_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
