47f892393a89a212582f3507b3a8410cc04c7556
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-09-15 15:28:26 +0300

    Update the upgrade script from 1.0.0 to 1.1.0

--- nostotagging/upgrade/upgrade-1.1.0.php
+++ nostotagging/upgrade/upgrade-1.1.0.php
@@ -7,6 +7,8 @@ if (!defined('_PS_VERSION_'))
  *
  * Creates 'nostotagging_customer_link' db table.
  * Registers hooks 'actionPaymentConfirmation' and 'displayPaymentTop'.
+ * Sets default value for "inject category and search page recommendations" to 1.
+ * Removes unused "NOSTOTAGGING_SERVER_ADDRESS" config variable.
  *
  * @param NostoTagging $object
  * @return bool
@@ -15,5 +17,7 @@ function upgrade_module_1_1_0($object)
 {
 	return $object->createCustomerLinkTable()
 		&& $object->registerHook('actionPaymentConfirmation')
-		&& $object->registerHook('displayPaymentTop');
+		&& $object->registerHook('displayPaymentTop')
+		&& $object->setInjectSlots(1, true)
+		&& Configuration::deleteByName('NOSTOTAGGING_SERVER_ADDRESS');
 }
