175fa065463093ee4c3fff8112527207db41e7b8
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-09-16 10:17:03 +0300

    Code cleanup and request failure logging. Add hook for recommendation elements on home page.

--- nostotagging/upgrade/upgrade-1.1.0.php
+++ nostotagging/upgrade/upgrade-1.1.0.php
@@ -6,7 +6,7 @@ if (!defined('_PS_VERSION_'))
  * Upgrades the module to version 1.1.0.
  *
  * Creates 'nostotagging_customer_link' db table.
- * Registers hooks 'actionPaymentConfirmation' and 'displayPaymentTop'.
+ * Registers hooks 'actionPaymentConfirmation', 'displayPaymentTop' and 'displayHome'.
  * Sets default value for "inject category and search page recommendations" to 1.
  * Removes unused "NOSTOTAGGING_SERVER_ADDRESS" config variable.
  *
@@ -18,6 +18,7 @@ function upgrade_module_1_1_0($object)
 	return $object->createCustomerLinkTable()
 		&& $object->registerHook('actionPaymentConfirmation')
 		&& $object->registerHook('displayPaymentTop')
+		&& $object->registerHook('displayHome')
 		&& $object->setInjectSlots(1, true)
 		&& Configuration::deleteByName('NOSTOTAGGING_SERVER_ADDRESS')
 		&& Configuration::deleteByName('NOSTOTAGGING_TOP_SELLERS_CMS_ID');
---
+++ nostotagging/views/templates/hook/home_nosto-elements.tpl
@@ -0,0 +1,4 @@
+<div class="nosto_element" id="frontpage-nosto-1"></div>
+<div class="nosto_element" id="frontpage-nosto-2"></div>
+<div class="nosto_element" id="frontpage-nosto-3"></div>
+<div class="nosto_element" id="frontpage-nosto-4"></div>
