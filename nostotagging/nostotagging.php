b4f5cd3e6ac54bc34f2e0bfe18f0696b47f414e0
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-10-13 10:01:24 +0300

    Fix order tagging

--- nostotagging/nostotagging.php
+++ nostotagging/nostotagging.php
@@ -1306,20 +1306,20 @@ class NostoTagging extends Module
 		$total_wrapping_tax_incl = 0;
 		$total_gift_tax_incl = 0;
 
-		// One order can be split into multiple orders, so we need to combine their data.
-		$order_collection = Order::getByReference($order->reference);
-		foreach ($order_collection as $item)
-		{
-			/** @var $item Order */
-			$products = array_merge($products, $item->getProducts());
-			$total_discounts_tax_incl = Tools::ps_round($total_discounts_tax_incl + $item->total_discounts_tax_incl, 2);
-			$total_shipping_tax_incl = Tools::ps_round($total_shipping_tax_incl + $item->total_shipping_tax_incl, 2);
-			$total_wrapping_tax_incl = Tools::ps_round($total_wrapping_tax_incl + $item->total_wrapping_tax_incl, 2);
-		}
-
-		// Cart rules are available from prestashop 1.5 onwards.
+		// Cart rules and split orders are available from prestashop 1.5 onwards.
 		if (_PS_VERSION_ >= '1.5')
 		{
+			// One order can be split into multiple orders, so we need to combine their data.
+			$order_collection = Order::getByReference($order->reference);
+			foreach ($order_collection as $item)
+			{
+				/** @var $item Order */
+				$products = array_merge($products, $item->getProducts());
+				$total_discounts_tax_incl = Tools::ps_round($total_discounts_tax_incl + $item->total_discounts_tax_incl, 2);
+				$total_shipping_tax_incl = Tools::ps_round($total_shipping_tax_incl + $item->total_shipping_tax_incl, 2);
+				$total_wrapping_tax_incl = Tools::ps_round($total_wrapping_tax_incl + $item->total_wrapping_tax_incl, 2);
+			}
+
 			// We need the cart rules used for the order to check for gift products and free shipping.
 			// The cart is the same even if the order is split into many objects.
 			$cart = new Cart($order->id_cart);
@@ -1360,11 +1360,19 @@ class NostoTagging extends Module
 			$items = array_merge($products, $gift_products);
 		}
 		else
-			$items = $products;
+		{
+			$products = $order->getProducts();
+			$total_discounts_tax_incl = $order->total_discounts;
+			$total_shipping_tax_incl = $order->total_shipping;
+			$total_wrapping_tax_incl = $order->total_wrapping;
 
-		$customer = $order->getCustomer();
+			$items = $products;
+		}
+
+		$customer = new Customer((int)$order->id_customer);
 		$nosto_order = array();
-		$nosto_order['order_number'] = (string)$order->reference;
+		// The order reference was introduced in prestashop 1.5 where orders can be split into multiple ones.
+		$nosto_order['order_number'] = isset($order->reference) ? (string)$order->reference : $order->id;
 		$nosto_order['buyer'] = array(
 			'first_name' => $customer->firstname,
 			'last_name' => $customer->lastname,
@@ -1407,12 +1415,13 @@ class NostoTagging extends Module
 
 		// Check is free shipping applies to the cart.
 		$free_shipping = false;
-		foreach ($cart_rules as $cart_rule)
-			if ((int)$cart_rule['free_shipping'])
-			{
-				$free_shipping = true;
-				break;
-			}
+		if (isset($cart_rules))
+			foreach ($cart_rules as $cart_rule)
+				if ((int)$cart_rule['free_shipping'])
+				{
+					$free_shipping = true;
+					break;
+				}
 
 		if (!$free_shipping && $total_shipping_tax_incl > 0)
 			$nosto_order['purchased_items'][] = array(
