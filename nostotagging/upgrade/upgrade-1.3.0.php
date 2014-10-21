670b1103936914b21d8e95767bf573eebc2e7253
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-10-21 10:16:20 +0300

    Update the 1.3 upgrade script

--- nostotagging/upgrade/upgrade-1.3.0.php
+++ nostotagging/upgrade/upgrade-1.3.0.php
@@ -5,7 +5,9 @@ if (!defined('_PS_VERSION_'))
 /**
  * Upgrades the module to version 1.3.0.
  *
+ * Move global config variables to language specific ones.
  * Removes unused config variables.
+ * Registers new hooks.
  *
  * @param NostoTagging $object
  * @return bool
@@ -30,17 +32,26 @@ function upgrade_module_1_3_0($object)
 		}
 	}
 
-	return Configuration::deleteByName('NOSTOTAGGING_DEFAULT_ELEMENTS')
-		// Register Prestashop 1.4 hooks.
-		&& $object->registerHook('header')
-		&& $object->registerHook('top')
-		&& $object->registerHook('footer')
-		&& $object->registerHook('productfooter')
-		&& $object->registerHook('shoppingCart')
-		&& $object->registerHook('orderConfirmation')
-		&& $object->registerHook('paymentConfirm')
-		&& $object->registerHook('paymentTop')
-		&& $object->registerHook('home')
-		// Register after object update hook (Prestashop >= 1.5).
-		&& $object->registerHook('actionObjectUpdateAfter');
+	Configuration::deleteByName('NOSTOTAGGING_DEFAULT_ELEMENTS');
+
+	// Backward compatibility
+	if (_PS_VERSION_ < '1.5')
+	{
+		$object->registerHook('header');
+		$object->registerHook('top');
+		$object->registerHook('footer');
+		$object->registerHook('productfooter');
+		$object->registerHook('shoppingCart');
+		$object->registerHook('orderConfirmation');
+		$object->registerHook('paymentConfirm');
+		$object->registerHook('paymentTop');
+		$object->registerHook('home');
+		$object->registerHook('updateproduct');
+		$object->registerHook('deleteproduct');
+		$object->registerHook('updateQuantity');
+	}
+	else
+		$object->registerHook('actionObjectUpdateAfter');
+
+	return true;
 }
