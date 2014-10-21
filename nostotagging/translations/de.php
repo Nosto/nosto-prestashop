4c6f234f3d3561208f4bbc768340834d796e7619
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-10-21 17:48:13 +0300

    Updated the translations and the configuration files.

---
+++ nostotagging/translations/de.php
@@ -0,0 +1,33 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{nostotagging}prestashop>nostotagging_4d7be0837e3660ec51a01652f028436b'] = 'Personalisierung für PrestaShop';
+$_MODULE['<{nostotagging}prestashop>nostotagging_2c71425adb350bace3a06ea8ff420eee'] = 'Steigern Sie Ihre Konvertierungsrate und den durschnittlichen Bestellwert durch das Bereitstellen von persönlichen Empfehlungen für Ihre Kunden während ihres Einkaufs.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_415e1f8adb980e44d0e0336e544bd9e1'] = 'Es wurden noch keine separaten Accounts für jede Sprache und jeden Shop erstellt.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_0b69722d7a4da2541d7a4ddad2a8bca5'] = 'Die Sprache darf nicht leer bleiben.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_a6e1f0fc86df0685fdd5372d5b5d276c'] = 'Emailadresse darf nicht leer bleiben.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_db7229b90398e80ac93ae00da09160d0'] = 'Die angegebene Email Adresse ist nicht korrekt.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_6166fa61da55ccf501caa157acb7d291'] = 'Das Nosto Konto konnte nicht automatisch erstellt werden. Bitte besuchen Sie nosto.com um ein neues Konto anzulegen.';
+$_MODULE['<{nostotagging}prestashop>nostotagging_f7806783dc72a64ebe11b06cc78dcd27'] = 'Das Nosto Konto wurde erstellt.';
+$_MODULE['<{nostotagging}prestashop>oauth2_cccb6184039fe4752063d680e45e54d9'] = ' Das Konto %s wurde erfolgreich mit Nosto angebunden.';
+$_MODULE['<{nostotagging}prestashop>oauth2_897ca8c00a599e39de66f9e9ce693872'] = 'Das Konto konnte nicht an Nosto angebunden werden. Bitte kontaktieren Sie den Nosto Support.';
+$_MODULE['<{nostotagging}prestashop>oauth2_ba242a643b1a9b3f42927e4fd1d10d16'] = 'Das Konto konnte nicht an Nosto angebunden werden. Sie haben die Verbindung abgewiesen.';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_222e64b935c7fc7670e4da20af7a6a5f'] = 'Sind Sie sicher, dass Sie Nosto deinstallieren möchten?';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_cd048faac86d87caf65a2b7d6177d513'] = 'Verwalten Sie Ihre Konten:';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_54d6c23d216046491d7b165a81265e85'] = 'Konto Setup';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_3effe4ed62b9777c2a55d5873c9e3bd5'] = 'Sie haben Nosto auf dem %1$s Shop installiert.';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_3b647f26882587fa6027a2b9ef0720d4'] = 'Ihre Konto ID lautet %s';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_9bd027289b839f4c8c5468a84ca6c1f6'] = 'Falls Sie Ihr Nosto Konto wechseln möchten, deinstallieren Sie zuerst Nosto.';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_0557fa923dcee4d0f86b1409f5c2167f'] = 'Zurück';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_9e1b344966535c9e9b90c1d73f2695a5'] = 'Deinstallation von Nosto';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_4ce8271b566183f60f0c5a76151b27b9'] = 'Installieren Sie Nosto im Shop';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_fb54f3c5992b96d001bb16e8e92d968d'] = '.';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_3348b4619d6a14d75519b8f459515eec'] = 'Haben Sie bereits ein Nostokonto erstellt?';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_93cba07454f06a4a960172bbd6e2a435'] = 'Ja';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Nein';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_260704261c0c6cd3c3834b6dc52e4d94'] = 'Mit Nosto verbinden';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'Email';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_1aa668d559a3a32a44796aeee6978a59'] = 'Neues Konto erstellen';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_aee2c9d4c9251a8156626cb783b2407c'] = 'Mit der Eröffnung des Kontos stimmen ';
+$_MODULE['<{nostotagging}prestashop>config-bootstrap_97abaa921b2270031573362f57aad4a4'] = 'Sie den Allgemeinen Geschäftsbedinungen von Nosto zu.';
