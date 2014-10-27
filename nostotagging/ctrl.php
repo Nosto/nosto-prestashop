aa78b0f6835f5187416744e0bd77d639b84d58c2
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-10-27 10:25:00 +0200

    Rafactoring

--- nostotagging/ctrl.php
+++ nostotagging/ctrl.php
@@ -1,13 +1,22 @@
 <?php
 
-// This script is meant to run outside of prestashop, so if the prestashop version is already defined, we do nothing.
+/*
+ * This is a backwards compatibility script for running module front controllers in Prestashop 1.4.
+ * The script is meant to run outside of Prestashop, so if _PS_VERSION_ is already defined, we do nothing.
+ */
 if (!defined('_PS_VERSION_'))
 {
-	require_once(dirname(__FILE__).'/../../config/config.inc.php');
-	// The ModuleFrontController class won't be defined in prestashop 1.4, so define it.
+	$ps_dir = dirname(__FILE__).'/../..';
+	require_once($ps_dir.'/config/config.inc.php');
+	/*
+	 * The "ModuleFrontController" class won't be defined in prestashop 1.4, so define it.
+	 */
 	if (_PS_VERSION_ < '1.5')
-		require_once(dirname(__FILE__).'/../../modules/nostotagging/controllers/front/ModuleFrontController.php');
-	$controller= Tools::getValue('controller');
-	require_once(dirname(__FILE__).'/../../modules/nostotagging/controllers/front/'.$controller.'.php');
-	ControllerFactory::getController('NostoTagging'.$controller.'ModuleFrontController')->run();
+		require_once($ps_dir.'/modules/nostotagging/backward_compatibility/ModuleFrontController.php');
+	$controller = strtolower((string)Tools::getValue('controller'));
+	if (!empty($controller))
+	{
+		require_once($ps_dir.'/modules/nostotagging/controllers/front/'.$controller.'.php');
+		ControllerFactory::getController('NostoTagging'.ucfirst($controller).'ModuleFrontController')->run();
+	}
 }
