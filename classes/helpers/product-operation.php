faa4a9c7d1deff97712107435d1992945034a6a9
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-05-20 13:56:53 +0300

    Fix shop context related issues in product updates

--- classes/helpers/product-operation.php
+++ classes/helpers/product-operation.php
@@ -25,9 +25,41 @@
 
 /**
  * Helper class for sending product create/update/delete events to Nosto.
+ *
+ * @todo there are a couple of issues with "multi-shop" installation URL generations:
+ *
+ * 1. The product page urls will not respect the "friendly urls" if one shop has it enabled and the other one does not.
+ * This will cause all urls to be either friendly or not, depending on what setting the first seen shop has. This seems
+ * to be caused by the `Dispatcher`.`use_routes` property that is set upon instance construction and cached due to the
+ * use of the Singleton pattern.
+ *
+ * 2. The product image urls will always point to the first seen shops url.
+ * This is due to the internal shop url cache handling and the fact the context cannot be overridden when populating it.
+ * The image urls also have an issue with the "friendly urls", which is due to the internal handling of the
+ * `Link`.`allow` property that is tied to the current context and cannot be overridden.
+ *
+ * The "friendly urls" issue SHOULD NOT be a real issue, as it is very unlikely the "friendly urls" setting would be
+ * enabled in one shop and disabled in another.
+ *
+ * The image url issue IS a real issue IF the product images are different in the shops. The image url will always point
+ * to an existing image, but that image may or may not be visible on the product page for the shop in question.
+ *
+ * How all this affects Nosto:
+ *
+ * 1. The "friendly url" issue can cause the products displayed in the recommendations to be broken if the shop does not
+ * have the setting enabled.
+ *
+ * 2. The product image displayed in the recommendation may not be visible on the product page it that shop.
  */
 class NostoTaggingHelperProductOperation
 {
+	/**
+	 * @var array runtime cache for products that have already been processed during this request to avoid sending the
+	 * info to Nosto many times during the same request. This will otherwise happen as PrestaShop will sometime invoke
+	 * the hook callback methods multiple times when saving a product.
+	 */
+	private static $_processed_products = array();
+
 	/**
 	 * Sends a product create API request to Nosto.
 	 *
@@ -35,12 +67,15 @@ class NostoTaggingHelperProductOperation
 	 */
 	public function create(Product $product)
 	{
-		if (!Validate::isLoadedObject($product))
+		if (!Validate::isLoadedObject($product) || in_array($product->id, self::$_processed_products))
 			return;
 
-		foreach ($this->getAccountsPerLanguage() as $id_lang => $account)
+		self::$_processed_products[] = $product->id;
+		foreach ($this->getAccountData() as $data)
 		{
-			$nosto_product = $this->loadNostoProduct((int)$product->id, $id_lang);
+			list($account, $id_shop, $id_lang) = $data;
+
+			$nosto_product = $this->loadNostoProduct((int)$product->id, $id_lang, $id_shop);
 			if (is_null($nosto_product))
 				continue;
 
@@ -69,12 +104,15 @@ class NostoTaggingHelperProductOperation
 	 */
 	public function update(Product $product)
 	{
-		if (!Validate::isLoadedObject($product))
+		if (!Validate::isLoadedObject($product) || in_array($product->id, self::$_processed_products))
 			return;
 
-		foreach ($this->getAccountsPerLanguage() as $id_lang => $account)
+		self::$_processed_products[] = $product->id;
+		foreach ($this->getAccountData() as $data)
 		{
-			$nosto_product = $this->loadNostoProduct((int)$product->id, $id_lang);
+			list($account, $id_shop, $id_lang) = $data;
+
+			$nosto_product = $this->loadNostoProduct((int)$product->id, $id_lang, $id_shop);
 			if (is_null($nosto_product))
 				continue;
 
@@ -103,11 +141,14 @@ class NostoTaggingHelperProductOperation
 	 */
 	public function delete(Product $product)
 	{
-		if (!Validate::isLoadedObject($product))
+		if (!Validate::isLoadedObject($product) || in_array($product->id, self::$_processed_products))
 			return;
 
-		foreach ($this->getAccountsPerLanguage() as $account)
+		self::$_processed_products[] = $product->id;
+		foreach ($this->getAccountData() as $data)
 		{
+			list($account) = $data;
+
 			$nosto_product = new NostoTaggingProduct();
 			$nosto_product->assignId($product);
 
@@ -130,49 +171,114 @@ class NostoTaggingHelperProductOperation
 	}
 
 	/**
-	 * Returns Nosto accounts mapped by their language ID for current shop.
+	 * Returns Nosto accounts based on active shops.
 	 *
-	 * @return NostoAccount[] the map.
+	 * The result is formatted as follows:
+	 *
+	 * array(
+	 *   array(object(NostoAccount), int(id_shop), int(id_lang))
+	 * )
+	 *
+	 * @return NostoAccount[] the account data.
 	 */
-	protected function getAccountsPerLanguage()
+	protected function getAccountData()
 	{
-		$accounts = array();
-		foreach (Language::getLanguages() as $language)
-		{
-			$id_lang = (int)$language['id_lang'];
-			/** @var NostoAccount $account */
-			$account = Nosto::helper('nosto_tagging/account')->find($id_lang);
-			if ($account === null || !$account->isConnectedToNosto())
-				continue;
+		$data = array();
+		/** @var NostoTaggingHelperAccount $account_helper */
+		$account_helper = Nosto::helper('nosto_tagging/account');
+		foreach ($this->getShops() as $shop) {
+			$id_shop = (int)$shop['id_shop'];
+			$id_shop_group = (int)$shop['id_shop_group'];
+			foreach (LanguageCore::getLanguages(true, $id_shop) as $language)
+			{
+				$id_lang = (int)$language['id_lang'];
+				$account = $account_helper->find($id_lang, $id_shop_group, $id_shop);
+				if ($account === null || !$account->isConnectedToNosto())
+					continue;
 
-			$accounts[$id_lang] = $account;
+				$data[] = array($account, $id_shop, $id_lang);
+			}
 		}
-		return $accounts;
+		return $data;
 	}
 
 	/**
-	 * Loads a Nosto product model for given PS product ID and language.
+	 * Returns the shops that are affected by the current context.
+	 *
+	 * @return array list of shop data.
+	 */
+	protected function getShops()
+	{
+		if (_PS_VERSION_ >= '1.5' && Shop::isFeatureActive() && Shop::getContext() !== Shop::CONTEXT_SHOP) {
+			if (Shop::getContext() === Shop::CONTEXT_GROUP)
+				return Shop::getShops(true, Shop::getContextShopGroupID());
+			else
+				return Shop::getShops(true);
+		} else {
+			$ctx = Context::getContext();
+			return array(
+				(int)$ctx->shop->id => array(
+					'id_shop' => (int)$ctx->shop->id,
+					'id_shop_group' => (int)$ctx->shop->id_shop_group,
+				),
+			);
+		}
+	}
+
+	/**
+	 * Loads a Nosto product model for given PS product ID, language ID and shop ID.
 	 *
 	 * @param int $id_product the PS product ID.
 	 * @param int $id_lang the language ID.
+	 * @param int $id_shop the shop ID.
 	 * @return NostoTaggingProduct|null the product or null if could not be loaded.
 	 */
-	protected function loadNostoProduct($id_product, $id_lang)
+	protected function loadNostoProduct($id_product, $id_lang, $id_shop)
 	{
-		$product = new Product($id_product, true, $id_lang);
+		$product = new Product($id_product, false, $id_lang, $id_shop);
 		if (!Validate::isLoadedObject($product))
 			return null;
 
-		if ($product->visibility === 'none')
+		if (isset($product->visibility) && $product->visibility === 'none')
 			return null;
 
 		$nosto_product = new NostoTaggingProduct();
-		$nosto_product->loadData(Context::getContext(), $product);
+		$nosto_product->loadData($this->makeContext($id_lang, $id_shop), $product);
 
 		$validator = new NostoModelValidator();
 		if (!$validator->validate($nosto_product))
 			return null;
 
 		return $nosto_product;
+	}
+
+	/**
+	 * Clones the current context and replace the info related to shop, language and currency.
+	 *
+	 * We need this when generating the product data for the different shops and languages.
+	 * The currency will be the first found for the shop, but it defaults to the PS default currency
+	 * if no shop specific one is found.
+	 *
+	 * @param int $id_lang the language ID to add to the new context.
+	 * @param int $id_shop the shop ID to add to the new context.
+	 * @return Context the new context.
+	 */
+	protected function makeContext($id_lang, $id_shop)
+	{
+		$ctx = Context::getContext()->cloneContext();
+		$ctx->language = new Language($id_lang);
+		$ctx->shop = new Shop($id_shop);
+		$currency = null;
+		if (_PS_VERSION_ >= '1.5')
+			foreach (Currency::getCurrenciesByIdShop($id_shop) as $row)
+				if ($row['deleted'] === '0' && $row['active'] === '1')
+				{
+					$currency = new Currency($row['id_currency']);
+					break;
+				}
+		if (is_null($currency))
+			$currency = Currency::getDefaultCurrency();
+		$ctx->currency = $currency;
+		return $ctx;
 	}
 }
--- classes/models/product.php
+++ classes/models/product.php
@@ -31,7 +31,6 @@ class NostoTaggingProduct extends NostoTaggingModel implements NostoProductInter
 	const IN_STOCK = 'InStock';
 	const OUT_OF_STOCK = 'OutOfStock';
 	const ADD_TO_CART = 'add-to-cart';
-	const OPTIMAL_IMAGE_WIDTH = 450;
 
 	/**
 	 * @var string absolute url to the product page.
@@ -123,6 +122,150 @@ class NostoTaggingProduct extends NostoTaggingModel implements NostoProductInter
 				'required',
 			)
 		);
+	}
+
+	/**
+	 * Loads the product data from supplied context and product objects.
+	 *
+	 * @param Context $context the context object.
+	 * @param Product $product the product object.
+	 */
+	public function loadData(Context $context, Product $product)
+	{
+		if (!Validate::isLoadedObject($product))
+			return;
+
+		/** @var NostoTaggingHelperUrl $url_helper */
+		$url_helper = Nosto::helper('nosto_tagging/url');
+
+		$id_lang = $context->language->id;
+		$id_shop = $context->shop->id;
+		$currency_iso_code = $context->currency->iso_code;
+
+		$this->url = $url_helper->getProductUrl($product, $id_lang, $id_shop);
+		$this->image_url = $url_helper->getProductImageUrl($product);
+		$this->product_id = (int)$product->id;
+		$this->name = $product->name;
+		$this->price = $this->calcPrice($product, $context);
+		$this->list_price = $this->calcPrice($product, $context, false/*no discounts*/);
+		$this->currency_code = strtoupper($currency_iso_code);
+		$this->availability = $this->checkAvailability($product);
+		$this->tags = $this->buildTags($product, $id_lang);
+		$this->categories = $this->buildCategories($product, $id_lang);
+		$this->short_description = $product->description_short;
+		$this->description = $product->description;
+		$this->brand = (!empty($product->manufacturer_name)) ? $product->manufacturer_name : null;
+		$this->date_published = Nosto::helper('date')->format($product->date_add);
+	}
+
+	/**
+	 * Assigns the product ID from given product.
+	 *
+	 * This method exists in order to expose a public API to change the ID.
+	 *
+	 * @param Product $product the product object.
+	 */
+	public function assignId(Product $product)
+	{
+		$this->product_id = (int)$product->id;
+	}
+
+	/**
+	 * Calculates the price (including tax if applicable) and returns it.
+	 *
+	 * We need to check if taxes are to be included in the prices, given that they are configured.
+	 * This is determined by the "Price display method" setting of the active user group.
+	 * Possible values are 1, tax excluded, and 0, tax included.
+	 *
+	 * @param Product $product the product model.
+	 * @param Context $context the context to calculate the price on (currency conversion).
+	 * @param bool $discounted_price if discounts should be applied.
+	 * @return string the calculated price.
+	 */
+	protected function calcPrice(Product $product, Context $context, $discounted_price = true)
+	{
+		$incl_tax = (bool)!Product::getTaxCalculationMethod((int)$context->cookie->id_customer);
+		$specific_price_output = null;
+		$value = Product::getPriceStatic(
+			(int)$product->id,
+			$incl_tax,
+			null, // $id_product_attribute
+			6, // $decimals
+			null, // $divisor
+			false, // $only_reduction
+			$discounted_price, // $user_reduction
+			1, // $quantity
+			false, // $force_associated_tax
+			null, // $id_customer
+			null, // $id_cart
+			null, // $id_address
+			$specific_price_output, // $specific_price_output
+			true, // $with_eco_tax
+			true, // $use_group_reduction
+			$context,
+			true // $use_customer_price
+		);
+		return Nosto::helper('price')->format($value);
+	}
+
+	/**
+	 * Checks the availability of the product and returns the "availability constant".
+	 *
+	 * The product is considered available if it is visible in the shop and is in stock.
+	 *
+	 * @param Product $product the product model.
+	 * @return string the value, i.e. self::IN_STOCK or self::OUT_OF_STOCK.
+	 */
+	protected function checkAvailability(Product $product)
+	{
+		$is_visible = (_PS_VERSION_ >= '1.5') ? ($product->visibility !== 'none') : true;
+		return ($product->checkQty(1) && $is_visible) ? self::IN_STOCK : self::OUT_OF_STOCK;
+	}
+
+	/**
+	 * Builds the tag list for the product.
+	 *
+	 * Also includes the custom "add-to-cart" tag if the product can be added to the shopping cart directly without
+	 * any action from the user, e.g. the product cannot have any variations or choices. This tag is then used in the
+	 * recommendations to render the "Add to cart" button for the product when it is recommended to a user.
+	 *
+	 * @param Product $product the product model.
+	 * @param int $id_lang for which language ID to fetch the product tags.
+	 * @return array the built tags.
+	 */
+	protected function buildTags(Product $product, $id_lang)
+	{
+		$tags = array();
+		if (($product_tags = $product->getTags($id_lang)) !== '')
+			$tags = explode(', ', $product_tags);
+
+		// If the product has no attributes (color, size etc.), then we mark it as possible to add directly to cart.
+		$product_attributes = $product->getAttributesGroups($id_lang);
+		if (empty($product_attributes))
+			$tags[] = self::ADD_TO_CART;
+
+		return $tags;
+	}
+
+	/**
+	 * Builds the category paths the product belongs to and returns them.
+	 *
+	 * By "path" we mean the full tree path of the products categories and sub-categories.
+	 *
+	 * @param Product $product the product model.
+	 * @param int $id_lang for which language ID to fetch the categories.
+	 * @return array the built category paths.
+	 */
+	protected function buildCategories(Product $product, $id_lang)
+	{
+		$categories = array();
+		foreach ($product->getCategories() as $category_id)
+		{
+			$category = NostoTaggingCategory::buildCategoryString($category_id, $id_lang);
+			if (!empty($category))
+				$categories[] = $category;
+		}
+		return $categories;
 	}
 
 	/**
@@ -235,106 +378,5 @@ class NostoTaggingProduct extends NostoTaggingModel implements NostoProductInter
 	public function getDatePublished()
 	{
 		return $this->date_published;
-	}
-
-	/**
-	 * Loads the product data from supplied context and product objects.
-	 *
-	 * @param Context $context the context object.
-	 * @param Product $product the product object.
-	 */
-	public function loadData(Context $context, Product $product)
-	{
-		if (!Validate::isLoadedObject($product))
-			return;
-
-		$language_id = $context->language->id;
-		$currency = $context->currency;
-		$link = $context->link;
-		$cookie = $context->cookie;
-
-		$this->url = (string)$product->getLink();
-		$this->product_id = (int)$product->id;
-		$this->name = (string)$product->name;
-
-		$image_id = $product->getCoverWs();
-		$image_type = $this->chooseOptimalImageType();
-		$this->image_url = (ctype_digit((string)$image_id) && !empty($image_type))
-			? (string)$link->getImageLink($product->link_rewrite, $product->id.'-'.$image_id, $image_type)
-			: '';
-
-		// We need to check if taxes are to be included in the prices, given that they are configured.
-		// This is determined by the "Price display method" setting of the active user group.
-		// Possible values are 1, tax excluded, and 0, tax included.
-		$price_display_method = Product::getTaxCalculationMethod((int)$cookie->id_customer);
-		$price = $product->getPrice((bool)!$price_display_method, null);
-		$list_price = $product->getPriceWithoutReduct((bool)$price_display_method, null);
-
-		$this->price = Nosto::helper('price')->format($price);
-		$this->currency_code = (string)$currency->iso_code;
-
-		$is_visible = (_PS_VERSION_ >= '1.5') ? ($product->visibility !== 'none') : true;
-		if ($product->checkQty(1) && $is_visible)
-			$this->availability = self::IN_STOCK;
-		else
-			$this->availability = self::OUT_OF_STOCK;
-
-		if (($tags = $product->getTags($language_id)) !== '')
-			$this->tags = explode(', ', $tags);
-
-		// If the product has no attributes (color, size etc.), then we mark it as possible to add directly to cart.
-		$product_attributes = $product->getAttributesGroups($language_id);
-		if (empty($product_attributes))
-			$this->tags[] = self::ADD_TO_CART;
-
-		foreach ($product->getCategories() as $category_id)
-		{
-			$category = NostoTaggingCategory::buildCategoryString($category_id, $language_id);
-			if (!empty($category))
-				$this->categories[] = (string)$category;
-		}
-
-		$this->short_description = (string)$product->description_short;
-		$this->description = (string)$product->description;
-		$this->list_price = Nosto::helper('price')->format($list_price);
-
-		if (!empty($product->manufacturer_name))
-			$this->brand = (string)$product->manufacturer_name;
-
-		$this->date_published = Nosto::helper('date')->format($product->date_add);
-	}
-
-	/**
-	 * Assigns the product ID from given product.
-	 *
-	 * @param Product $product the product object.
-	 */
-	public function assignId(Product $product)
-	{
-		$this->product_id = (int)$product->id;
-	}
-
-	/**
-	 * Chooses the "optimal" image type to use for product image urls.
-	 *
-	 * The type is chosen based on which image type has a width closest to `self::OPTIMAL_IMAGE_WIDTH`.
-	 *
-	 * @return string|false the image type name or false if not found.
-	 */
-	protected function chooseOptimalImageType()
-	{
-		$definition = (_PS_VERSION_ >= '1.5') ? ObjectModel::getDefinition('ImageType') : array();
-		$table_name = isset($definition['table']) ? $definition['table'] : 'image_type';
-		$available_image_types = Db::getInstance()->executeS('
-			SELECT * FROM `'._DB_PREFIX_.pSQL($table_name).'`
-			WHERE `products` = 1
-			ORDER BY `width` ASC
-		');
-		$optimal = self::OPTIMAL_IMAGE_WIDTH;
-		$found = array();
-		foreach ($available_image_types as $available)
-			if (empty($found) || abs($optimal - (int)$found['width']) > abs((int)$available['width'] - $optimal))
-				$found = $available;
-		return isset($found['name']) ? $found['name'] : false;
 	}
 }
