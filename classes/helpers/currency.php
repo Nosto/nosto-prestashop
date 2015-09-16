b6fd0822ec8b9fb52b4496bffabb9eed0fbd02b0
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-09-16 11:15:28 +0300

    Fix currency getter for PS 1.4

--- classes/helpers/currency.php
+++ classes/helpers/currency.php
@@ -65,13 +65,7 @@ class NostoTaggingHelperCurrency
 		if (_PS_VERSION_ >= '1.5')
 			return Currency::getCurrenciesByIdShop($id_shop);
 		else
-			return Db::getInstance()->executeS('
-				SELECT *
-				FROM `'._DB_PREFIX_.'currency` c
-				LEFT JOIN `'._DB_PREFIX_.'currency_shop` cs ON (cs.`id_currency` = c.`id_currency`)
-				WHERE cs.`id_shop` = '.$id_shop.'
-				ORDER BY `name` ASC'
-			);
+			return Currency::getCurrencies();
 	}
 
 	/**
