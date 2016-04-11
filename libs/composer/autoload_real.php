71914cacde4ec0731041af69523eeb1e8794d450
Hannu Pölönen <hannu.polonen@nosto.com>
2016-04-11 13:46:33 +0300

    Add phing packaging and re-arrange the file structure to meet new Prestashop requirements

--- libs/composer/autoload_real.php
+++ libs/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit85abbc1b0ac0fc49a7aa746daace9509
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit85abbc1b0ac0fc49a7aa746daace9509', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
