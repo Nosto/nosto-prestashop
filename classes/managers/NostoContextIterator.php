a2cd2abfe88a8350201dfef8cf7f4d0ed4350878
Mridang Agarwalla <mridang@nosto.com>
2017-08-11 14:20:16 +0300

    Improved the context iterator

--- classes/managers/NostoContextIterator.php
+++ classes/managers/NostoContextIterator.php
@@ -45,15 +45,7 @@ class NostoContextIterator implements IteratorAggregate
         return true;
     }
 
-    /**
-     * Retrieve an external iterator
-     *
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return Traversable An instance of an object implementing <b>Iterator</b> or
-     * <b>Traversable</b>
-     * @since 5.0.0
-     */
     public function getIterator()
     {
-        // TODO: Implement getIterator() method.
+        return new ArrayIterator($this->scopes);
 }}
