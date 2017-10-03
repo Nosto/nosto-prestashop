4f18d8ac84dd478eac567bb56c62459d7321c80f
Chen Liangde <chen.liangde@nosto.com>
2017-10-03 11:49:05 +0300

    Format code

--- classes/helpers/NostoHelperPrice.php
+++ classes/helpers/NostoHelperPrice.php
@@ -159,8 +159,10 @@ class NostoHelperPrice
                 //A hack to fix the multi-currency issue. Function Tools::convertPrice() caches the
                 //default currency. If multi-store is enabled and default currencies are different in
                 //different stores, it cause problem. Big number 1000,000 is used to avoid rounding issue.
-                $exchangeRate = Tools::convertPrice(1000000, (int)Configuration::get('PS_CURRENCY_DEFAULT'))
-                    / 1000000;
+                $exchangeRate = Tools::convertPrice(
+                    1000000,
+                    (int)Configuration::get('PS_CURRENCY_DEFAULT')
+                ) / 1000000;
                 $value *= $exchangeRate;
 
                 return NostoHelperPrice::roundPrice($value);
