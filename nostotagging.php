57f513b3505c90cee2a9343ea83fbe4aef166a5d
Chen Liangde <chen.liangde@nosto.com>
2018-06-28 15:42:07 +0300

    Update version number and change log

--- nostotagging.php
+++ nostotagging.php
@@ -56,7 +56,7 @@ class NostoTagging extends Module
      *
      * @var string
      */
-    const PLUGIN_VERSION = '3.4.2';
+    const PLUGIN_VERSION = '3.5.0';
 
     /**
      * Internal name of the Nosto plug-in
