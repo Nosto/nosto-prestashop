db805abdccc0c923b11a461faea81460080f8a29
Cid Lopes <alannettto@gmail.com>
2018-10-04 15:19:50 +0300

    Add Extra Array Key Check
    Returns Earlier If Product Is Already Available

--- classes/models/NostoProduct.php
+++ classes/models/NostoProduct.php
@@ -255,12 +255,15 @@ class NostoProduct extends NostoSDKProduct
         if (!$product->active || $product->visibility === 'none') {
             return self::INVISIBLE;
         }
+        if ($product->checkQty(1)) {
+            return self::IN_STOCK;
+        }
         $combinations = $product->getAttributeCombinations();
         if (empty($combinations)) {
             return $product->checkQty(1) ? self::IN_STOCK : self::OUT_OF_STOCK;
         }
         foreach ($combinations as $combination) {
-            if ($combination['quantity'] > 0) {
+            if (array_key_exists('quantity', $combination) && $combination['quantity'] > 0) {
                 return self::IN_STOCK;
             }
         }
