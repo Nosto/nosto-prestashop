cabe29fd4a5d630891f6bf5ebc1e0709f1f23511
Olsi Qose <olsi.qose@nosto.com>
2020-06-18 10:06:07 +0300

    Update copyright header year

--- classes/models/NostoBrand.php
+++ classes/models/NostoBrand.php
@@ -19,7 +19,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    Nosto Solutions Ltd <contact@nosto.com>
- * @copyright 2013-2019 Nosto Solutions Ltd
+ * @copyright 2013-2020 Nosto Solutions Ltd
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
