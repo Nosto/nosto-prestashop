9630492d9b5b8ca971280eae07a70e0bdd21fed8
Cid Lopes <alannettto@gmail.com>
2020-06-23 16:53:59 +0300

    Fix namespace in PageTypeTagging

--- classes/blocks/NostoPageTypeTagging.php
+++ classes/blocks/NostoPageTypeTagging.php
@@ -23,7 +23,7 @@
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
-use Nosto\NostoException;
+use \Nosto\NostoException;
 use \Nosto\Object\PageType as NostoSDKPageType;
 
 class NostoPageTypeTagging
