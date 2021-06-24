a38ffec4cca299eb4ac5d12d70e8446a73b6fc7e
olsi-qose <olsi.qose@nosto.com>
2021-06-24 16:18:05 +0300

    Bump version & update changelog

--- nostotagging.php
+++ nostotagging.php
@@ -55,7 +55,7 @@ class NostoTagging extends Module
      *
      * @var string
      */
-    const PLUGIN_VERSION = '3.9.3';
+    const PLUGIN_VERSION = '3.9.4';
 
     /**
      * Internal name of the Nosto plug-in
