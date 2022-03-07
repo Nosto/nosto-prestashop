fb9dc91a47d836ba141b14b540fad49a0934c78a
Cid Lopes <alannettto@gmail.com>
2022-03-07 17:25:02 +0200

    Remove PII data

--- classes/models/NostoOrder.php
+++ classes/models/NostoOrder.php
@@ -74,7 +74,7 @@ class NostoOrder extends NostoSDKOrder
             $nostoOrder->setOrderNumber((string)$order->id);
         }
         $nostoOrder->setOrderNumber(isset($order->reference) ? (string)$order->reference : $order->id);
-        $customer = NostoOrderBuyer::loadData($customer, $order);
+        $customer = NostoOrderBuyer::loadData($customer);
         if ($customer instanceof NostoSDKBuyer) {
             $nostoOrder->setCustomer($customer);
         }
--- classes/models/order/NostoOrderBuyer.php
+++ classes/models/order/NostoOrderBuyer.php
@@ -31,42 +31,16 @@ class NostoOrderBuyer extends NostoSDKOrderBuyer
      * Loads the buyer data from the customer object.
      *
      * @param Customer $customer the customer model to process
-     * @param Order $order the order object
      * @return NostoOrderBuyer|null the buyer object
      * @throws PrestaShopException
      */
-    public static function loadData(Customer $customer, Order $order)
+    public static function loadData(Customer $customer)
     {
         if (!NostoHelperConfig::isCustomerTaggingEnabled()) {
             return null;
         }
         $nostoBuyer = new NostoOrderBuyer();
-        $nostoBuyer->setFirstName($customer->firstname);
-        $nostoBuyer->setLastName($customer->lastname);
-        $nostoBuyer->setEmail($customer->email);
         $nostoBuyer->setMarketingPermission($customer->newsletter);
-
-        $billingAddressId = $order->id_address_invoice;
-        $addresses = $customer->getAddresses($order->id_lang);
-        if ($addresses) {
-            foreach ($addresses as $address) {
-                if (array_key_exists('id_address', $address)
-                    && $address['id_address'] === $billingAddressId
-                ) {
-                    $addressObject = new Address($address['id_address'], $order->id_lang);
-                    if ($addressObject->phone_mobile) {
-                        $nostoBuyer->setPhone($addressObject->phone_mobile);
-                    } elseif ($addressObject->phone) {
-                        $nostoBuyer->setPhone($addressObject->phone);
-                    }
-                    $nostoBuyer->setPostcode($addressObject->postcode);
-                    if (array_key_exists('id_country', $address)) {
-                        $nostoBuyer->setCountry(Country::getIsoById($address['id_country']));
-                    }
-                    break;
-                }
-            }
-        }
 
         NostoHelperHook::dispatchHookActionLoadAfter(get_class($nostoBuyer), array(
             'customer' => $customer,
