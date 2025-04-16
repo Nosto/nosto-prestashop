847f163c7c6e84e819c1c0549b2a087b9cacbe8a
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2025-04-16 09:04:44 +0300

    merge conflicts

--- nostotagging.php
+++ nostotagging.php
@@ -55,7 +55,7 @@ class NostoTagging extends Module
      *
      * @var string
      */
-    const PLUGIN_VERSION = '4.4.2';
+    const PLUGIN_VERSION = '4.4.3';
 
     /**
      * Internal name of the Nosto plug-in
